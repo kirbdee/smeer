d0b2f448fb5d8a29448baaf999db9e94fdfeff0f
Kirby Domingo <kirby.domingo@gmail.com>
2016-04-05 00:48:27 -0700

    v0.1 - initial MVP

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var ig = require('../lib/instagram.js');
+
+/* GET users listing. */
+router.get('/:username', function (req, res, next) {
+    //IG api need to take in "error" callback in case IG fails
+    ig.getUserInfo(req.params.username,function(data){
+        res.render('users', data);
+    })
+
+});
+
+module.exports = router;
