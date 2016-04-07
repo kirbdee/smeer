dddc8dd079d63dbc508bfdd390be29b7facfac35
Kirby Domingo <kirby.domingo@gmail.com>
2016-04-06 21:55:55 -0700

    Removed GA hard code, added conf, removed adsense unit

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+__GLOBAL_ENV__ = require('./conf/conf');
+
 var app = express();
 
 // view engine setup
---
+++ conf/conf.js
@@ -0,0 +1,4 @@
+module.exports = {
+    GA_ID: process.env.GA_ID || null,
+    NODE_ENV: process.env.NODE_ENV || 'development'
+};
